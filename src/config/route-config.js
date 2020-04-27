091352edebb871087ae83452769cdc8a31e40c42
Layne <layneingramtaylor@gmail.com>
2020-04-26 20:57:58 -0500

    static route fixed

--- src/config/route-config.js
+++ src/config/route-config.js
@@ -1,10 +1,11 @@
+const staticRoutes = require('../routes/static');
+const listRoutes = require('../routes/lists');
+const itemRoutes = require('../routes/items');
+
 module.exports = {
   init(app) {
-    const staticRoutes = require("../routes/static");
-    const listRoutes = require("../routes/lists");
-    const itemRoutes = require("../routes/items");
     app.use(staticRoutes);
     app.use(listRoutes);
     app.use(itemRoutes);
-  }
+  },
 };
--- src/routes/lists.js
+++ src/routes/lists.js
@@ -1,11 +1,12 @@
-const express = require("express");
+const express = require('express');
+
 const router = express.Router();
 
-const listController = require("../controllers/listController");
+const listController = require('../controllers/listController');
 
-router.get("/lists", listController.index);
-router.get("/lists/new", listController.new);
-router.post("/lists/create", listController.create);
-router.get("/lists/:id", listController.show);
+router.get('/lists', listController.index);
+router.get('/lists/new', listController.new);
+router.post('/lists/create', listController.create);
+router.get('/lists/:id', listController.show);
 
 module.exports = router;
