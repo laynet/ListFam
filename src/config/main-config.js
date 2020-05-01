32547817dc68b2f7c096ca79c241f14e2613a338
Layne <layneingramtaylor@gmail.com>
2020-05-01 16:10:22 -0500

    sign up form displaying

--- src/config/main-config.js
+++ src/config/main-config.js
@@ -3,12 +3,14 @@ const path = require('path');
 
 const viewsFolder = path.join(__dirname, '..', 'views');
 const bodyParser = require('body-parser');
+const expressValidator = require('express-validator');
 
 module.exports = {
   init(app, express) {
     app.set('views', viewsFolder);
     app.set('view engine', 'ejs');
     app.use(bodyParser.urlencoded({ extended: true }));
+    app.use(expressValidator());
     app.use(express.static(path.join(__dirname, '..', 'assets')));
   },
 };
--- src/config/route-config.js
+++ src/config/route-config.js
@@ -1,11 +1,13 @@
 const staticRoutes = require('../routes/static');
 const listRoutes = require('../routes/lists');
 const itemRoutes = require('../routes/items');
+const userRoutes = require('../routes/users');
 
 module.exports = {
   init(app) {
     app.use(staticRoutes);
     app.use(listRoutes);
     app.use(itemRoutes);
+    app.use(userRoutes);
   },
 };
